b9a7dfef3bc87f5ba0ff0ad1739a8817ff94fe89
N R Mitra <nileshranjan.mitra@yahoo.in>
2017-01-04 20:41:42 +0530

    AUTH_SERVICE is hooked up now webApi starts

--- Presentation/StaticContents/scripts/login/LoginController.js
+++ Presentation/StaticContents/scripts/login/LoginController.js
@@ -3,10 +3,21 @@
 *	$scope - > child scope object created by the angular framework
 *	AUTHENTICATION_STATE -> service (which in turn object) created through  constant receipe
 *	AUTHENTICATION_SERVICE -> service (which in turn object) created through  factory receipe
+
+*	Controller initiate first and then view has been rendered, thus need to define the "credential" object first
+*	that has been used in the view later
 */
 app.controller("loginController",function($scope,AUTHENTICATION_STATE,AUTHENTICATION_SERVICE)
 {
-	alert(AUTHENTICATION_STATE.loginSuccess);
+	$scope.credential = {
+		userid : "",
+		password : ""
+	};
+	$scope.Validate = function(userCredential){
+		alert(userCredential.userid);
+		alert(userCredential.password);
+	}
+	alert(AUTHENTICATION_SERVICE);
 }
 );
 
@@ -14,7 +25,6 @@ app.controller("loginController",function($scope,AUTHENTICATION_STATE,AUTHENTICA
 *
 */
 app.factory("AUTHENTICATION_SERVICE",function(AUTHENTICATION_STATE){
-	alert(AUTHENTICATION_STATE.loginFailed);
-	return AUTHENTICATION_STATE.loginFailed;
+		return AUTHENTICATION_STATE.loginSuccess+" for Nilesh";
 }
 );
